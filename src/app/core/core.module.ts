ce41281090a7cd2fe95251899965c313e8dc9425
Etienne Blanc-Coquand <e.blanc@leviatan.fr>
2019-08-19 15:37:44 +0200

    Add countdown component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from './layout/layout.module';
+import { CountdownComponent } from './countdown/countdown.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CountdownComponent],
   imports: [
     CommonModule,
     LayoutModule
