b770f1760cfa436b75b757d3e64253ce56aaf6d4
Etienne Blanc-Coquand <e.blanc@leviatan.fr>
2019-08-19 15:35:07 +0200

    Fix errors

--- src/app/pages/stories/stories.module.ts
+++ src/app/pages/stories/stories.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [],
   imports: [
     CommonModule,
+    HomeModule
   ]
 })
 export class StoriesModule { }
