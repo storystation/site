a74828e727b0491eab4d17cef10d3781311019e7
Etienne Blanc-Coquand <e.blanc@leviatan.fr>
2019-08-20 16:46:57 +0200

    Create game mode, add first example of game card (with color choice)

--- src/app/pages/stories/stories.routing.ts
+++ src/app/pages/stories/stories.routing.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
 export const storiesRoutes: Routes = [
   {
     path: 'stories',
     component: HomeComponent
+  },
+  {
+    path: 'stories/game',
+    component: GameComponent
   }
 ];
