5630823059d2bb600f6511e3ff7165d5acc3a98f
Etienne Blanc-Coquand <e.blanc@leviatan.fr>
2019-08-20 17:07:52 +0200

    Add aria-label and countdown

--- src/app/pages/stories/game/game.module.ts
+++ src/app/pages/stories/game/game.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameComponent } from './game.component';
+import { CountdownModule } from 'ngx-countdown';
 
 @NgModule({
   declarations: [GameComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CountdownModule
   ]
 })
 export class GameModule { }
