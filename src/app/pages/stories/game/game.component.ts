a8da4057af93e44da09b7b3849521c4a96545209
Etienne Blanc-Coquand <e.blanc@leviatan.fr>
2019-08-20 17:29:56 +0200

    Add interfaces for color buttons and interractions

--- src/app/pages/stories/game/game.component.ts
+++ src/app/pages/stories/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import GameColor from '../../../../shared/interfaces/game-color';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  isColorButtons = true;
+  isRadar = false;
+
+  gameColors: GameColor[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.definesGameColorsAvailable();
   }
 
+  definesGameColorsAvailable() {
+    this.gameColors.push({name: 'Red', is_clickable: true});
+    this.gameColors.push({name: 'Blue', is_clickable: true});
+    this.gameColors.push({name: 'Yellow', is_clickable: true});
+    this.gameColors.push({name: 'Green', is_clickable: true});
+  }
+
+  /**
+   * Actions to do when the time limit is up.
+   */
+  onFinished() {
+    console.log('Temps imparti dépassé');
+    this.setRandomResults();
+  }
+
+  /**
+   * Defines a random result, calls a specific function according to the module that is used for the current game.
+   */
+  setRandomResults() {
+    console.log('Génération d\'un résultat aléatoire');
+    if (this.isColorButtons) {
+      this.clickRandomButton();
+    } else if (this.isRadar) {
+      this.setRandomDistanceRadar();
+    }
+  }
+
+  /**
+   * Selects a random button based on all available buttons
+   */
+  clickRandomButton() {
+    const max = this.gameColors.length;
+    const element = Math.floor(Math.random() * Math.floor(max));
+    console.log('Le bouton ' + this.gameColors[element].name + ' a été cliqué');
+  }
+
+  setRandomDistanceRadar() {
+    console.log('Définition aléatoire d\'une distance pour le jeu avec le radar');
+  }
 }
---
+++ src/shared/interfaces/game-color.ts
@@ -0,0 +1,4 @@
+export default interface GameColor {
+  name: string;
+  is_clickable: boolean;
+}
