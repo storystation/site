9e338a50220885954dae6a929d11cd9d83744b36
Etienne Blanc-Coquand <e.blanc@leviatan.fr>
2019-08-21 11:07:54 +0200

    Create service for service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UpdateService } from './services/update.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  constructor(
+    private update: UpdateService
+  ) {}
   title = 'Storystation';
 }
---
+++ src/app/services/update.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material';
+import { SwUpdate } from '@angular/service-worker';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateService {
+  constructor(
+    private swUpdate: SwUpdate,
+    private snackbar: MatSnackBar
+  ) {
+    !this.swUpdate.isEnabled ? console.log('SW Not enabled') : console.log('SW Enabled');
+    this.swUpdate.available.subscribe(evt => {
+      const snack = this.snackbar.open('Mise à jour disponible', 'Reload');
+      snack.onAction().subscribe(() => {
+        window.location.reload();
+      });
+      /*snack.setTimeout(() => {
+       snack.dismiss();
+       }, 6000);*/
+    });
+  }
+}
