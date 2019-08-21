91643aa23178731ae6e159eb8ee015bf5d625fb6
Etienne Blanc-Coquand <e.blanc@leviatan.fr>
2019-08-21 12:23:32 +0200

    Replace the comp to redirection

--- src/app/pages/landing/landing.routing.ts
+++ src/app/pages/landing/landing.routing.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { LandingComponent } from './landing.component';
-import { HomeComponent } from '../stories/home/home.component';
 
 export const landingRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    redirectTo: '/stories',
+    pathMatch: 'full'
   },
   {
     path: 'prices',
